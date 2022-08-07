f48c15e7c3c2c54c5e8cf569ba12c12e98c7c11e
3jun <kk3june@gmail.com>
2022-08-07 23:31:19 +0900

    [FEAT] Todos 리스트 랜더링 및 추가 버튼 기능구현

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [token, setToken] = useState();
+  const [todoTitle, setTodoTitle] = useState();
+  const [todoContent, setTodoContent] = useState();
+  const [todos, setTodos] = useState([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    setToken(window.localStorage.getItem("token"));
+
+    axios({
+      method: "GET",
+      url: "http://localhost:8080/todos",
+      headers: {
+        Authorization: `Basic ${token}`,
+      },
+    }).then((res) => setTodos(res.data.data));
+  }, [token, todoTitle, todoContent]);
+
+  function onSubmit(data) {
+    setTodoTitle(data.title);
+    setTodoContent(data.content);
+
+    axios({
+      method: "POST",
+      url: "http://localhost:8080/todos",
+      headers: {
+        Authorization: `Basic ${token}`,
+      },
+      data: {
+        title: data.title,
+        content: data.content,
+      },
+    }).then((res) => console.log(res));
+  }
+
+  return (
+    <div>
+      <TodoSection>
+        {todos &&
+          todos.map((el) => {
+            return (
+              <div key={el.title}>
+                <span>Title : {el.title}</span>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span>Content : {el.content}</span>
+              </div>
+            );
+          })}
+        <form>
+          <input placeholder="title" {...register("title")} />
+          <input placeholder="content" {...register("content")} />
+        </form>
+        <TodoBtn onClick={handleSubmit(onSubmit)}>추가</TodoBtn>
+        <TodoBtn>수정</TodoBtn>
+        <TodoBtn>삭제</TodoBtn>
+      </TodoSection>
+    </div>
+  );
 };
 
 export default Home;
+
+const TodoSection = styled.div``;
+const TodoBtn = styled.button``;
