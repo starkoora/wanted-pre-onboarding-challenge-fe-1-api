76ccee0777fd00f368bbaf4c9b220868615579aa
Woongi Han <69628701+wgnator@users.noreply.github.com>
2022-08-13 02:00:28 +0900

    Update todoService.ts

--- services/todoService.ts
+++ services/todoService.ts
@@ -19,7 +19,7 @@ export const findTodo = (predicate: (todo: Todo) => boolean) => {
 };
 
 export const updateTodo = async (todo: Todo, todoValue: Partial<Todo>) => {
-  Object.assign(todo, update<Todo>({ ...todo, todoValue })); 
+  Object.assign(todo, update<Todo>({ ...todo, ...todoValue })); 
 
   await db.write();
 
