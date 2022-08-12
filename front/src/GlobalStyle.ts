a02aa7d2cd813fe248cf42ef55aa46124164cefd
3jun <kk3june@gmail.com>
2022-08-13 00:22:08 +0900

    [CHORE] GlobalStyle 적용

---
+++ front/src/GlobalStyle.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    body{
+        padding: 0;
+        margin: 0;
+        font-family: 'Noto Sans KR', sans-serif;
+    };
+`;
+
+export default GlobalStyle;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./GlobalStyle";
 import App from "./App";
+import GlobalStyle from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
