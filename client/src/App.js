a51aa133246e34bb34566e378ecc003e9288874f
hmyo2853 <hmyo2853@gmail.com>
2023-01-02 23:46:28 +0900

    feat: react-router-dom install & auth 페이지 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Auth from "./Pages/Auth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ client/src/Pages/Auth.js
@@ -0,0 +1,5 @@
+const Auth = () => {
+  return <>로그인 / 회원가입 페이지</>;
+};
+
+export default Auth;
