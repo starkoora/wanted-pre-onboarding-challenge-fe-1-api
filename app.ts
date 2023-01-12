848c20816a6cb4ce4506f88730d1bc7ee4abb8c7
박겸영 <yong_drg@naver.com>
2023-01-12 15:01:21 +0900

    add: Access-Control-Allow-Origin header

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import type { Request, Response, NextFunction } from "express";
-import cors from "cors";
-import createError from "http-errors";
-import { StatusCodes } from "http-status-codes";
+import express from 'express';
+import type { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import createError from 'http-errors';
+import { StatusCodes } from 'http-status-codes';
 
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 
-import todoRouter from "./routes/todoRouter";
-import userRouter from "./routes/userRouter";
+import todoRouter from './routes/todoRouter';
+import userRouter from './routes/userRouter';
 
 const app = express();
 
@@ -15,24 +15,17 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors({ origin: 'https://main--seoltang-todo.netlify.app/' }));
 
-app.use("/todos", todoRouter);
-app.use("/users", userRouter);
+app.use('/todos', todoRouter);
+app.use('/users', userRouter);
 
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use(
-  (
-    err: Error & { status: number },
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
-  }
-);
+app.use((err: Error & { status: number }, req: Request, res: Response, next: NextFunction) => {
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
+});
 
 export default app;
